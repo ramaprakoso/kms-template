41937bdf682c8a1e0434ca4b9ddb48ca16836a16
ramaprakoso <ramadhan.prakoso@isid.co.id>
2018-06-19 10:00:33 +0700

    patching wiki & document

---
+++ js/index.js
@@ -0,0 +1,8 @@
+ $("#login-button").click(function(){
+		 // event.preventDefault();
+	 // $().attr("https://www.w3schools.com/jquery/");
+	 // $('form').fadeOut(500);
+	 // $('.wrapper').addClass('form-success');
+   window.location = "index.html";
+
+});
